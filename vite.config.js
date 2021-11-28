52a030b5cda4d913184a251fdfdcbfc4881fd668
Valentin Bailleul <8208027+Baloche@users.noreply.github.com>
2021-11-28 01:25:42 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
-export default {
-    base: ''
-}
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        stage: resolve(__dirname, 'stage.html')
+      }
+    }
+  }
+})
